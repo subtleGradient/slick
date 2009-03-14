28a8f0c5fd4771bbee8586a39612aed6291cb6b4
Thomas Aylott <oblivious+git@subtleGradient.com>
2009-03-14 00:07:51 -0400

    specs proving combinator parsing is broken

--- test/specs.js
+++ test/specs.js
@@ -1,17 +1,21 @@
-(function(){
+String.escapeSingle = function escapeSingle(string){
+	return (''+string).replace(/(?=[\\\n'])/g,'//');
+}
+
+// (function(){
 	var SubtleSlickParse_Specs = {
 		
 		'should exist': function(){
 			value_of( SubtleSlickParse ).should_not_be_undefined();
-		},
-		
+		}
+		,
 		'should parse multiple selectors': function(){
 			var s = SubtleSlickParse('a, b, c');
 			value_of( s[0][0].tag ).should_be( 'a' );
 			value_of( s[1][0].tag ).should_be( 'b' );
 			value_of( s[2][0].tag ).should_be( 'c' );
-		},
-		
+		}
+		,
 		'should parse multiple selectors with class': function(){
 			var s = SubtleSlickParse('a.class, b.class, c.class');
 			value_of( s[0][0].tag ).should_be( 'a' );
@@ -20,27 +24,14 @@
 			value_of( s[0][0].classes[0] ).should_be( 'class' );
 			value_of( s[1][0].classes[0] ).should_be( 'class' );
 			value_of( s[2][0].classes[0] ).should_be( 'class' );
-		},
-		
+		}
+		,
 		'should parse tag names': function(){
 			for (var i=0; i < tags.length; i++) {var tag = tags[i];
 				value_of( SubtleSlickParse(tag)[0][0].tag ).should_be( tag );
 			}
-		},
-		
-		'should parse tag names with combinators': function(){
-			for (var i=0; i < tags.length; i++) {var tag = tags[i];
-				for (var C=0; C < combinators.length; C++) {var combinator = combinators[C];
-					
-					var s = SubtleSlickParse(tag +combinator+ tag);
-					
-					value_of( s[0][0].tag ).should_be( tag );
-					value_of( s[0][1].tag ).should_be( tag );
-					value_of( s[0][1].combinator ).should_be( combinator );
-				}
-			}
-		},
-		
+		}
+		,
 		'should transform odd to 2n+1 in pseudos nth arguments': function(){
 			var nths = [
 				{raw:        ":nth-child(odd)", name:       "nth-child", argument:"2n+1"},
@@ -50,15 +41,15 @@
 				
 				{raw:        ":nth-child(odd)", name:'nth-child', argument:"2n+1"},
 				{raw:       ":nth-child(2n+1)", name:'nth-child', argument:"2n+1"},
-				{raw:          ":nth-child(n)", name:'nth-child', argument:"n"   },
+				{raw:          ":nth-child(n)", name:'nth-child', argument:"n"   }
 			];
 			for (var i=0,s, N; N = nths[i]; i++){
 				s = SubtleSlickParse(N.raw);
 				value_of( s[0][0].pseudos[0].name ).should_be( N.name );
 				value_of( s[0][0].pseudos[0].argument ).should_be( N.argument );
 			}
-		},
-
+		}
+		,
 		'should transform even to 2n in pseudo nth arguments': function(){
 			var nths = [
 				{raw:        ":nth-child(even)", name:       "nth-child", argument:"2n"},
@@ -67,8 +58,8 @@
 				{raw:      ":nth-of-type(even)", name:     "nth-of-type", argument:"2n"},
 				
 				{raw:":nth-child(even)", name:'nth-child', argument:"2n" },
-				{raw:":nth-child(2n)"  , name:'nth-child', argument:"2n"  },
-				{raw:":nth-child(n)"   , name:'nth-child', argument:"n"   },
+				{raw:":nth-child(2n)"  , name:'nth-child', argument:"2n" },
+				{raw:":nth-child(n)"   , name:'nth-child', argument:"n"  }
 			];
 			for (var i=0,s, N; N = nths[i]; i++){
 				s = SubtleSlickParse(N.raw);
@@ -112,20 +103,36 @@
 		}
 		,
 		'should parse attributes': function(){ }
-	}
+	};
 	
-	var combinators = ' ,>,+,~'.split(',');
-	var tags = 'a abbr acronym address applet area b base basefont bdo big blockquote br button caption center cite code col colgroup dd del dfn dir div dl dt em fieldset font form frame frameset h1 h2 h3 h4 h5 h6 head hr html i iframe img input ins isindex kbd label legend li link map menu meta noframes noscript object ol optgroup option p param pre q s samp script select small span strike strong style sub sup table tbody td textarea tfoot th thead title tr tt u ul var'.split(' ');
+	var combinators = ' ,>,+,~,   , > , + , ~ '.split(',');
+	// var tags = 'a abbr acronym address applet area b base basefont bdo big blockquote br button caption center cite code col colgroup dd del dfn dir div dl dt em fieldset font form frame frameset h1 h2 h3 h4 h5 h6 head hr html i iframe img input ins isindex kbd label legend li link map menu meta noframes noscript object ol optgroup option p param pre q s samp script select small span strike strong style sub sup table tbody td textarea tfoot th thead title tr tt u ul var'.split(' ');
+	var tags = 'a abbr div A ABBR DIV'.split(' ');
 	var attribOperators = ' = != *= ^= $= ~= |='.split(' ');
 	var attrs = 'attr html:lang fred-rocks'.split(' ');
 	var vals = 'myValueOfDoom;"double";\'single\';();{};\'thing[]\';"thing[]"'.split(';');
+	
+	// 'should parse tag names with combinators': function(){
+	// };
+	
+	for (var C=0; C < combinators.length; C++) {var combinator = combinators[C];
+		for (var i=0; i < tags.length; i++) {var tag = tags[i];
+			
+			var functionString = '\n';
+			functionString += "var s = SubtleSlickParse('"+String.escapeSingle(tag + combinator + tag)+"');\n";
+			
+			functionString+="value_of( s[0][0].tag ).should_be( '"+String.escapeSingle(tag)+"' );\n" +
+							"value_of( s[0][1].tag ).should_be( '"+String.escapeSingle(tag)+"' );\n" +
+							"value_of( s[0][1].combinator ).should_be( '"+String.escapeSingle(combinator.length===3 ? combinator[1] : combinator)+"' );\n";
+			
+			SubtleSlickParse_Specs['should parse '+tag+' tags with "'+combinator+'" combinator' ] = new Function(functionString);
+		}
+	}
 	
 	vals: for (var vi=0; vi < vals.length; vi++) {
 		var val = vals[vi];
 		attrs: for (var ai=0; ai < attrs.length; ai++) {
 			var attr = attrs[ai];
-			
-			// SubtleSlickParse_Specs['should parse attributes: '+ attr +' & val:' + val] = function(){
 			
 			operators: for (var i=0; i < attribOperators.length; i++) {
 				var op = attribOperators[i];
@@ -145,21 +152,17 @@
 						value_of( s.attributes[0].value ).should_be( val.replace(/^["']|['"]$/g,'') );
 						value_of( s.attributes[0].regexp.toString() ).should_be( SubtleSlickParse.attribValueToRegex(op, op&&val.replace(/^["']|['"]$/g,'')).toString() );
 					}
-				}
+				};
 			}
 		}
 	}
 	
 	describe('SubtleSlickParse', SubtleSlickParse_Specs);
-})();
+// })();
 
 
 var s,f,kid,template;
 (function(SlickFindingSpecs){
-	
-	String.escapeSingle = function escapeSingle(string){
-		return string.replace(/(?=[\\\n'])/g,'//');
-	}
 	
 	function makeSlickTest(selector, count) {
 		if (document.querySelectorAll)
@@ -170,6 +173,9 @@ var s,f,kid,template;
 	var count, selector, selectors = {
 		
 		// tags
+		'html':1,
+		'body':1,
+		'head':1,
 		'div':59,
 		
 		// ids
@@ -200,14 +206,10 @@ var s,f,kid,template;
 		':not([class=""])':1817,
 		
 		'':0
-	}
+	};
 	for (selector in selectors) {
 		
 		SlickFindingSpecs['should find '+selectors[selector]+' "'+selector+'"' ] = makeSlickTest(selector, selectors[selector]);
-		
-		// SlickFindingSpecs['should find as many "'+selector+'" as qsa' ] = function(){
-		// 	value_of( slick(document, selector).length ).should_be( document.querySelectorAll(selector).length );
-		// };
 		
 	};
 	
