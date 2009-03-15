a81fa64ec0f2675d408033932d11651efef5918b
Thomas Aylott <oblivious+git@subtleGradient.com>
2009-03-15 17:57:33 -0400

    more specs!

--- test/specs.js
+++ test/specs.js
@@ -34,6 +34,12 @@ function makeSlickTestAttrib(attr, op, val) {
 	return new Function(functionString);
 }
 
+function makeSlickTestSearch(selector, count) {
+	// if (document.querySelectorAll)
+	// return new Function(" var count; try{ count = document.querySelectorAll('"+String.escapeSingle(selector)+"').length; console.log('"+String.escapeSingle(selector)+"', count); }catch(e){ \ncount="+count+" }; value_of( slick(document, '"+String.escapeSingle(selector)+"').length ).should_be( count );");
+	return new Function(" value_of( slick(document, '"+String.escapeSingle(selector)+"').length ).should_be( "+count+" );");
+}
+
 // SubtleSlickParse.debug = true;
 
 var combinators = ' ,>,+,~,   , > , + , ~ '.split(',');
@@ -61,10 +67,11 @@ var vals = 'myValueOfDoom;"double";\'single\';();{};\'thing[]\';"thing[]"'.split
 	
 	
 	for (var C=0; C < combinatorsSpecial.length; C++) {var combinator = combinatorsSpecial[C];
-		for (var i=0; i < tags.length; i++) {var tag = tags[i];
+		tag = tag2 = tags[0];
+		// for (var i=0; i < tags.length; i++) {var tag = tags[i];
 			
 			SubtleSlickParse_Specs['should parse '+tag+' tags with "'+combinator+'" custom combinator' ] = makeSlickTestCombinator(tag, combinator, tag);
-		}
+		// }
 	}
 	
 	describe('SubtleSlickParse Custom Combinators', SubtleSlickParse_Specs);
@@ -184,11 +191,11 @@ var vals = 'myValueOfDoom;"double";\'single\';();{};\'thing[]\';"thing[]"'.split
 		}
 	}
 	
-	vals: for (var vi=0; vi < vals.length; vi++) {
+	/*vals:*/ for (var vi=0; vi < vals.length; vi++) {
 		var val = vals[vi];
-		attrs: for (var ai=0; ai < attrs.length; ai++) {
+		/*attrs:*/ for (var ai=0; ai < attrs.length; ai++) {
 			var attr = attrs[ai];
-			operators: for (var oi=0; oi < attribOperators.length; oi++) {
+			/*operators:*/ for (var oi=0; oi < attribOperators.length; oi++) {
 				var op = attribOperators[oi];
 				
 				SubtleSlickParse_Specs['should parse attributes: '+ '['+ attr + op + (op&&val) +']'] = makeSlickTestAttrib(attr, op, val);
@@ -203,52 +210,130 @@ var vals = 'myValueOfDoom;"double";\'single\';();{};\'thing[]\';"thing[]"'.split
 var s,f,kid,template;
 (function(SlickFindingSpecs){
 	
-	function makeSlickTest(selector, count) {
-		if (document.querySelectorAll)
-		return new Function(" var count; try{ count = document.querySelectorAll('"+String.escapeSingle(selector)+"').length; console.log('"+String.escapeSingle(selector)+"', count); }catch(e){ \ncount="+count+" }; value_of( slick(document, '"+String.escapeSingle(selector)+"').length ).should_be( count );");
-		return new Function(" value_of( slick(document, '"+String.escapeSingle(selector)+"').length ).should_be( "+count+" );");
-	}
-	
 	var count, selector, selectors = {
+		'html': 1,
+		'body': 1,
+		'head': 1,
+		'div': 59,
 		
-		// tags
-		'html':1,
-		'body':1,
-		'head':1,
-		'div':59,
+		'.example': 43,
+		'.note': 14,
+		'.fn': 5,
 		
-		// ids
+		'.a1': 4,
+		'.a1 .a1': 2,
+		'.a1   .a1': 2,
+		'.a1 > .a1': 2,
+		'.a1 + .a1': 0,
+		'.a1 * .a1': 0,
+		'.a4': 4,
+		'.a4   .a4': 2,
+		'.a4 > .a4': 2,
+		'.a4 + .a4': 0,
+		'.a4 * .a4': 0,
 		
+		'[class]': 324,
+		'[class=]': 0,
+		'[class=""]': 0,
+		'[class!=]': 324,
+		'[class!=""]': 324,
+		'[class]:not([class=""])': 324,
+		':not([class=""])': 1825,
 		
-		// classes
-		'.example':43,
-		'.note':14,
-		'.fn':5,
+		'[title]': 13,
+		'[title=]': 0,
+		'[title=""]': 0,
+		'[title!=]': 13,
+		'[title!=""]': 13,
+		'[title]:not([title=""])': 13,
+		':not([title=""])': 1825,
 		
-		'.a1':4,
-		'.a1 .a1':2,
-		'.a1   .a1':2,
-		'.a1 > .a1':2,
-		'.a1 + .a1':0,
-		'.a1 * .a1':0,
+		'body div': 59,
+		'div p': 140,
+		'div > p': 134,
+		'div + p': 22,
+		'div ~ p': 183,
 		
-		'.a4':4,
-		'.a4   .a4':2,
-		'.a4 > .a4':2,
-		'.a4 + .a4':0,
-		'.a4 * .a4':0,
+		'div[class^=exa][class$=mple]': 43,
+		'div p a': 12,
+		'div, p, a': 683,
 		
-		// class attribs
-		'[class]':325,
-		'[class=]':1, '[class=""]':1,
-		'[class!=]':324, '[class!=""]':324, '[class]:not([class=""])':324,
-		':not([class=""])':1817,
+		'div.example': 43,
+		'ul .tocline2': 12,
+		'div.example, div.note': 44,
 		
-		'':0
+		'#title': 1,
+		'h1#title': 1,
+		'div #title': 1,
+		
+		'ul.toc li.tocline2': 12,
+		'ul.toc > li.tocline2': 12,
+		'h1#title + div > p': 0,
+		
+		// contains
+		// var count = [];Array.prototype.slice.call($$('*')).forEach(function(el){ el.innerHTML.search(/Selectors/)+1 && count.push(el); });count
+		'h1[id]:contains(Selectors)': 1,
+		'h1[id]:contains("Selectors")': 1,
+		':contains(Selectors)': 58,
+		':contains("Selectors")': 58,
+		'p:contains(selectors)': 22,
+		'p:contains("selectors")': 22,
+		
+		// attribs
+		'[href][lang][class]': 1,
+		'[class]': 55,
+		'[class=example]': 43,
+		'[class^=exa]': 43,
+		'[class$=mple]': 43,
+		'[class*=e]': 54,
+		'[lang|=tr]': 1,
+		'[class!=made_up]': 59,
+		'[class~=example]': 43,
+		
+		// pseudos
+		'div:not(.example)': 16,
+		'p:nth-child(even)': 158,
+		'p:nth-child(2n)': 158,
+		'p:nth-child(odd)': 166,
+		'p:nth-child(2n+1)': 166,
+		'p:nth-child(n)': 324,
+		'p:only-child': 3,
+		'p:last-child': 19,
+		'p:first-child': 54,
+		
+		"":0
 	};
+	
+	SlickFindingSpecs['":contains()" elements should really contain the word'] = function(){
+		var els = document.search(':contains(selectors)');
+		for (var i=0,el; el=els[i]; i++) value_of( el.innerHTML ).should_match( 'selectors' );
+		
+		var els = document.search(':contains(Selectors)');
+		for (var i=0,el; el=els[i]; i++) value_of( el.innerHTML ).should_match( 'Selectors' );
+		
+	};
+	
+	SlickFindingSpecs['Get QSA Results'] = function(){
+		var qsa_results = ['{\n'], count;
+		for (selector in selectors) {
+			qsa_results.push("\t'");
+			qsa_results.push(String.escapeSingle(selector));
+			qsa_results.push("': ");
+			try{
+				count = document.querySelectorAll(selector).length;
+			}catch(e){
+				count = -1;
+			}
+			qsa_results.push(count);
+			qsa_results.push(',\n');
+		}
+		qsa_results.push('\t"":0\n}');
+		console.log(qsa_results.join(''));
+	};
+	
 	for (selector in selectors) {
 		
-		SlickFindingSpecs['should find '+selectors[selector]+' "'+selector+'"' ] = makeSlickTest(selector, selectors[selector]);
+		SlickFindingSpecs['should find '+selectors[selector]+' "'+selector+'"' ] = makeSlickTestSearch(selector, selectors[selector]);
 		
 	};
 	
@@ -269,13 +354,13 @@ var s,f,kid,template;
 		s = s || document.getElementById('jsspec_container');
 		s.parentNode.removeChild(s);
 		
-		while (kid = template.childNodes[0]){
+		while ((kid = template.childNodes[0])){
 			document.body.appendChild(kid);
 		}
 	},
 	
 	after: function(){
-		while (kid = document.body.childNodes[0]){
+		while ((kid = document.body.childNodes[0])){
 			template.appendChild(kid);
 		}
 		
