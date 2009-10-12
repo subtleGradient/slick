e5d3f9f11fe07016c1ed15aabd7272b97919be90
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-10-12 02:44:46 -0400

    Slick Selector Engine Exhaustive: CLASS

--- test/slick_engine_specs.js
+++ test/slick_engine_specs.js
@@ -441,3 +441,63 @@ Describe('Slick Selector Engine',function(){
 	it_should_find(43,'[class~=example]');
 	
 });
+
+Describe('Slick Selector Engine Exhaustive',function(){
+	
+	var CLASSES = "normal escaped\\,character ǝpoɔıun 瀡 with-dash with_underscore 123number MiXeDcAsE".split(' ');
+	Describe('CLASS',function(){
+		
+		var testNode;
+		specs.before_all = function(){
+			testNodeOrphaned = context.document.createElement('div');
+			testNode = context.document.createElement('div');
+			context.document.body.appendChild(testNode);
+		};
+		specs.after_all = function(){
+			testNode && testNode.parentNode && testNode.parentNode.removeChild(testNode);
+			testNode = null;
+		};
+		
+		var it_should_select_classes = function(CLASSES){
+			
+			var testName = 'Should select "'+ CLASSES.join(' ') +'"';
+			var className = CLASSES.join(' ');
+			if (className.indexOf('\\')+1) className += ' ' + CLASSES.join(' ').replace('\\','');
+			
+			function build(){
+				testNodeOrphaned.innerHTML = testNode.innerHTML = '<div></div><div class="'+ className +'"><div></div></div><div></div>';
+			};
+			
+			it[testName + ' from the document root'] = function(){
+				build();
+				result = context.Slick(testNode.ownerDocument, '.' + CLASSES.join('.'));
+				value_of( result.length ).should_be( 1 );
+				value_of( result[0].className ).should_match( CLASSES.join(' ') );
+			};
+			
+			it[testName + ' from the parent'] = function(){
+				build();
+				var result = context.Slick(testNode, '.' + CLASSES.join('.'));
+				value_of( result.length ).should_be( 1 );
+				value_of( result[0].className ).should_match( CLASSES.join(' ') );
+			};
+			
+			it[testName + ' orphaned'] = function(){
+				build();
+				result = context.Slick(testNodeOrphaned, '.' + CLASSES.join('.'));
+				value_of( result.length ).should_be( 1 );
+				value_of( result[0].className ).should_match( CLASSES.join(' ') );
+			};
+			
+			// it should match this class as a second class
+			if (CLASSES.length == 1) it_should_select_classes(['foo',CLASSES[0]]);
+		};
+		
+		it_should_select_classes(CLASSES);
+		for (var i=0; i < CLASSES.length; i++)
+			it_should_select_classes([CLASSES[i]]);
+		
+	});
+	
+});
+
