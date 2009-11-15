e75cd3fd9e3c1d8b1e1c4bffd7d9c77a5ec1dca4
Jan Kassens <jan@kassens.net>
2009-11-16 00:40:03 +0100

    adding some specs for parsed.type

--- test/slick_parser_specs.js
+++ test/slick_parser_specs.js
@@ -44,6 +44,16 @@ Describe('Slick Parser',function(){
 		}
 	};
 	
+	it['should set the type'] = function(){
+		value_of(PARSE('.class1').type).should_be(['className']);
+		value_of(PARSE('.class1.class2').type).should_be(['classNames']);
+		value_of(PARSE('.class1.class2.class3.class4').type).should_be(['classNames']);
+		value_of(PARSE('#id.class1.class2').type).should_be(['id', 'classNames']);
+		value_of(PARSE('div:foo(bar):not(td)').type).should_be(['tagName', 'pseudoClass', 'pseudoClass']);
+		value_of(PARSE('a b').type).should_be(['tagName', 'combinatorChildren', 'tagName']);
+		value_of(PARSE('a > b.bar').type).should_be(['tagName', 'combinator', 'tagName', 'className']);
+	}
+
 });
 
 Describe('Slick Parser Syntax',function(){
