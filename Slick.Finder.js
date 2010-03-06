129460b5b9e174818f8060bb69b0f41c2e5c18d5
Fabio M. Costa <fabiomcosta@gmail.com>
2010-03-06 15:47:20 -0300

    Revert "SPEEDUP impressive how changing from call to a simple method call improved the speed"
    This reverts commit fd5beb6e9a0b9ad235affc90715b3d6486c582b3.

--- Slick.Finder.js
+++ Slick.Finder.js
@@ -180,7 +180,7 @@ local.search = function(context, expression, append, first){
 		for (i = this.overrides.length; i--;){
 			var override = this.overrides[i];
 			if (override.regexp.test(expression)){
-				var result = override.method(context, expression, found, first);
+				var result = override.method.call(context, expression, found, first);
 				if (result === false) continue;
 				if (result === true) return found;
 				return result;
@@ -635,14 +635,14 @@ local.override = function(regexp, method){
 	local.overrides.push({regexp: regexp, method: method});
 };
 
-local.override(/./, function(context, expression, found, first){ //querySelectorAll override
+local.override(/./, function(expression, found, first){ //querySelectorAll override
 
-	if (!context.querySelectorAll || context.nodeType != 9 || local.isXMLDocument || local.brokenMixedCaseQSA || Slick.disableQSA) return false;
+	if (!this.querySelectorAll || this.nodeType != 9 || local.isXMLDocument || local.brokenMixedCaseQSA || Slick.disableQSA) return false;
 	
 	var nodes, node;
 	try {
-		if (first) return context.querySelector(expression) || null;
-		else nodes = context.querySelectorAll(expression);
+		if (first) return this.querySelector(expression) || null;
+		else nodes = this.querySelectorAll(expression);
 	} catch(error){
 		return false;
 	}
@@ -661,11 +661,11 @@ local.override(/./, function(context, expression, found, first){ //querySelector
 
 });
 
-local.override(/^[\w-]+$|^\*$/, function(context, expression, found, first){ // tag override
+local.override(/^[\w-]+$|^\*$/, function(expression, found, first){ // tag override
 	var tag = expression;
 	if (tag == '*' && local.starSelectsComments || local.starSelectsClosed) return false;
 	
-	var nodes = context.getElementsByTagName(tag);
+	var nodes = this.getElementsByTagName(tag);
 	
 	if (first) return nodes[0] || null;
 	var i, node, hasOthers = !!(found.length);
@@ -679,12 +679,12 @@ local.override(/^[\w-]+$|^\*$/, function(context, expression, found, first){ // 
 	return true;
 });
 
-local.override(/^\.[\w-]+$/, function(context, expression, found, first){ // class override
+local.override(/^\.[\w-]+$/, function(expression, found, first){ // class override
 	if (local.isXMLDocument) return false;
 	
 	var nodes, node, i, hasOthers = !!(found && found.length), className = expression.substring(1);
-	if (context.getElementsByClassName && !local.cachedGetElementsByClassName && !local.brokenSecondClassNameGEBCN){
-		nodes = context.getElementsByClassName(className);
+	if (this.getElementsByClassName && !local.cachedGetElementsByClassName && !local.brokenSecondClassNameGEBCN){
+		nodes = this.getElementsByClassName(className);
 		if (first) return nodes[0] || null;
 		for (i = 0; node = nodes[i++];){
 			if (!hasOthers || !local.uniques[node.uniqueNumber || (node.uniqueNumber = local.uidx++)]){
@@ -693,7 +693,7 @@ local.override(/^\.[\w-]+$/, function(context, expression, found, first){ // cla
 		}
 	} else {
 		var matchClass = new RegExp('(^|\\s)'+ Slick.parse.escapeRegExp(className) +'(\\s|$)');
-		nodes = context.getElementsByTagName('*');
+		nodes = this.getElementsByTagName('*');
 		for (i = 0; node = nodes[i++];){
 			if (!matchClass.test(node.className)) continue;
 			if (first) return node;
@@ -704,10 +704,10 @@ local.override(/^\.[\w-]+$/, function(context, expression, found, first){ // cla
 	return (first) ? null : true;
 });
 
-local.override(/^#[\w-]+$/, function(context, expression, found, first){ // ID override
-	if (local.isXMLDocument || !context.getElementById) return false;
+local.override(/^#[\w-]+$/, function(expression, found, first){ // ID override
+	if (local.isXMLDocument || !this.getElementById) return false;
 	
-	var id = expression.substring(1), el = context.getElementById(id);
+	var id = expression.substring(1), el = this.getElementById(id);
 	if (!el) return found;
 	if (local.idGetsName && el.getAttributeNode('id').nodeValue != id) return false;
 	if (first) return el || null;
