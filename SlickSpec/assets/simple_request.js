ba67ba21950361e6001e772c75abc6ad38aea9e0
Fabio M. Costa <fabiomcosta@gmail.com>
2010-09-24 00:17:44 -0300

    Making possible for ie to parse xml files even if it doesnt come with correct content-type

--- SlickSpec/assets/simple_request.js
+++ SlickSpec/assets/simple_request.js
@@ -1,8 +1,36 @@
 var SimpleRequest = (function(){
 	
+	/**
+	 * Parse the XML document contained in the string argument and return
+	 * a Document object that represents it.
+	 */
+	var parseXML = (function(){
+		// Mozilla, Firefox, and related browsers
+		return (typeof DOMParser != "undefined") ? function(rawXML){
+			return (new DOMParser()).parseFromString(rawXML, "application/xml");
+		} : (typeof ActiveXObject != "undefined") ? function(rawXML){
+			var xmlDocument = new ActiveXObject("Microsoft.XMLDOM");
+			xmlDocument.async = false;
+			xmlDocument.loadXML(rawXML);
+			if (xmlDocument.parseError && xmlDocument.parseError.errorCode)
+				xmlDocument.loadXML(rawXML.replace(/<!DOCTYPE[^>]*?>/i,''));
+			if (xmlDocument.parseError && xmlDocument.parseError.errorCode)
+			throw new Error([''
+				,("Error code: " + xmlDocument.parseError.errorCode)
+				,("Error reason: " + xmlDocument.parseError.reason)
+				,("Error line: " + xmlDocument.parseError.line)
+				,rawXML
+			].join('\n'));
+			return xmlDocument;
+		} : function(){
+			return null;
+		};
+	})();
+	
 	function SimpleRequest(){
 		this.initialize();
 	};
+	
 	SimpleRequest.prototype = {
 		
 		initialize: function(){
@@ -11,7 +39,7 @@ var SimpleRequest = (function(){
 		
 		createXHR: function(){
 			// return ('XMLHttpRequest' in window)? new XMLHttpRequest(): new ActiveXObject('MSXML2.XMLHTTP');
-			return ('XMLHttpRequest' in window)? new XMLHttpRequest(): new ActiveXObject('Microsoft.XMLHTTP');
+			return ('XMLHttpRequest' in window) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
 		},
 		
 		stateChange: function(fn){
